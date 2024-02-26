1c52d7fcc1803df47c58f2fa98ebd877bb2a3b7e
hirakawa masaki <hirakawamasaki@hirakawanoiMac.local>
2024-02-27 00:10:44 +0900

    idをパス変数で受け取るように実装

--- src/main/java/com/work/kadai8/MemberController.java
+++ src/main/java/com/work/kadai8/MemberController.java
@@ -1,7 +1,7 @@
 package com.work.kadai8;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -15,9 +15,13 @@ public class MemberController {
         this.memberMapper = memberMapper;
     }
 
-
     @GetMapping("/member")
-    public List<Member> findById(@RequestParam int id) {
+    public List<Member> getAll() {
+        return memberMapper.getAll();
+    }
+
+    @GetMapping("/member/{id}")
+    public List<Member> findById(@PathVariable("id") int id) {
         return memberMapper.findByMemberId(id);
     }
 }
