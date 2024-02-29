5ce20c9a2e37b51ea1952f6b4f28b62b71c6dac6
hirakawa masaki <hirakawamasaki@hirakawanoiMac.local>
2024-02-29 23:46:23 +0900

    ServiceとExceptionHandlerの実装

---
+++ src/main/java/com/work/kadai8/MemberNotFoundException.java
@@ -0,0 +1,8 @@
+package com.work.kadai8;
+
+public class MemberNotFoundException extends RuntimeException {
+    public MemberNotFoundException(String message) {
+        super(message);
+
+    }
+}
---
+++ src/main/java/com/work/kadai8/MemberService.java
@@ -0,0 +1,29 @@
+package com.work.kadai8;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+    private MemberMapper memberMapper;
+
+    public MemberService(MemberMapper memberMapper) {
+        this.memberMapper = memberMapper;
+    }
+
+    public List<Member> findAll() {
+        return memberMapper.getAll();
+    }
+
+
+    public Member findById(int id) {
+        Optional<Member> member = this.memberMapper.findByMemberId(id);
+        if (member.isPresent()) {
+            return member.get();
+        } else {
+            throw new MemberNotFoundException("member not found");
+        }
+    }
+}
